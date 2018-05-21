903f013f17a5ed45daafb57e0617b7fbaf8b410d
Marin <marin_stephanov@abv.bg>
2018-05-22 00:34:40 +0300

    burger-app: l 208

--- react/udemy/burger-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ react/udemy/burger-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 
 const CheckoutSummary = (props) => (
-    
+    <div className={classes.CheckoutSummary}>
+        <h1>Леко, да не се задавиш!</h1>
+        <div className={classes.BurgerCt}>
+            <Burger ingredients={props.ingredients} />
+        </div>
+        <Button type='Danger'>CANCEL</Button>
+        <Button type='Success'>CONTINUE</Button>
+    </div>
 );
 
 export default CheckoutSummary;
