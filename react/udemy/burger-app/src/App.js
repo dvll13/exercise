ecbdf8225ea5195571e6a154ec1d106856363e37
Marin <marin_stephanov@abv.bg>
2018-05-23 19:52:13 +0300

    burger-app: 208 unfinished

--- react/udemy/burger-app/src/App.js
+++ react/udemy/burger-app/src/App.js
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route} from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <BurgerBuilder/>
-                <Route path='/checkout' component={Checkout}/>
+                <Switch>
+                    {/* TODO: fix basename */}
+                    <Route path='/' exact component={BurgerBuilder} />
+                    <Route path='/checkout' component={Checkout} />
+                    <Redirect to='/' />
+                </Switch>
             </Layout>
         )
     }
