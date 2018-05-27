8ffb308ce8f13d4354c4689d8b72d59ae6d26e16
Marin <marin_stephanov@abv.bg>
2018-05-27 14:49:09 +0300

    redux-app: l 242

---
+++ react/udemy/redux-app/redux-basics.js
@@ -0,0 +1,38 @@
+const redux = require('redux');
+const createStre = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        // NEVER MUTATE any data!
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+
+    return state;
+}
+
+// Store
+const store = createStre(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+// store.dispatch({type: 'INC_COUNTER', payload: { value: 1, ... }});
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
+
+// Subscription
