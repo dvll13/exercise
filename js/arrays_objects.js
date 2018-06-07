46a8492b824d68902d0df39694e51b4034d25c9a
Marin <marin_stephanov@abv.bg>
2018-06-07 18:22:33 +0300

    arrays_objects additions

--- js/arrays_objects.js
+++ js/arrays_objects.js
@@ -30,6 +30,49 @@ for (var i in associative_array) { console.log(i) };
 }
 
 
+{ // more useful methods
+    let a = [];
+
+    a.filter( age => age > 18 );
+    // Creates a new array based on whether the items of an array pass a certain condition.
+
+    a.map( number => '$' + number );
+    // Creates a new array by manipulating the values in another array. Great for data manipulation and it is often used in React because it is an immutable method
+
+    a.reduce( (accumulator, currentValue) => accumulator + currentValue );
+    // This often overlooked method uses an accumulator to reduce all items in an array to a single value. Great for calculating totals. The returned value can be of any type (i.e. object, array, string, integer)
+
+    a.forEach( emotion => console.log(emotion) );
+    // Applies a function on each item in an array
+
+    a.some( element => element === 'admin');
+    // Checks if any item in an array passes the condition. A good use case would be checking for user privileges. It can also be used similarly to a .forEach() where you would perform an action on each array item and break out of the loop once a truthy value is returned
+
+    a.every( rating => rating >= 3 );
+    // Similar to .some(), but checks if all items in an array pass a condition
+
+    a.includes('waldo');
+    // Checks if an array contains a certain value. It’s similar to .some(), but instead of looking for a condition to pass, it looks if the array contains a specific value
+}
+
+{ // ARRAY SPREAD
+    //Spreading arrays using the spread operator (…) allows you to expand the elements in an array. It’s useful when concatenating a bunch of arrays together. It’s also a good way to avoid using the splice() method when looking to remove certain elements from an array because it can be combined with the slice() method to prevent direct mutation of an array.
+
+    // example: combine two arrays
+    const spreadableOne = [1, 2, 3, 4];
+    const spreadableTwo = [5, 6, 7, 8];
+
+    const combined = [...spreadableOne, ...spreadableTwo];
+    // combined will be equal to [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+    // example: remove an array element without mutating the original array
+    const animals = ['squirrel', 'bear', 'deer', 'salmon', 'rat'];
+    const mammals = [...animals.slice(0,3), ...animals.slice(4)];
+    // mammals will be equal to ['squirrel', 'bear', 'deer', 'rat']
+}
+
+
 var a = new Object();
 a.one = 'aaa';
 a.two = 'bbb';
@@ -64,3 +107,55 @@ console.log(window.myVar); // → 10
 if (prop in obj) {}
 for (let prop in obj) {}
 // The in operator can be used to find out whether an object contains a property with a given name. The same keyword can also be used in a for loop (for (var name in object)) to loop over an object’s properties.
+
+{ // useful methods
+    let o = {}, o2 = {};
+
+    o.values();
+    // Return an array of the values of an object
+
+    o.keys();
+    // Return an array of the keys of an object
+
+    o.entries();
+    // Creates an array which contains arrays of key/value pairs of an object
+
+    Object.freeze(o)
+    // Prevents you from modifying existing object properties or adding new properties and values to an object. It’s often what people think const does, however const allows you to modify an object.
+
+    Object.seal(o)
+    // Stops any new properties from being added to an object, but still allows for existing properties to be changed.
+
+    Object.assign(o, o2)
+    // Allows for objects to be combined together. This method is no longer needed because you can use the object spread syntax instead. Like the object spread operator, Object.assign() does not do deep cloning. Lodash is your best friend when it comes to deep cloning objects.
+}
+
+{ // OBJECT SPREAD
+    // Spreading an object allows for the addition of new properties and values to an object without mutations (i.e. a new object is created) and it can also be used to combine multiple objects together. It should be noted that spreading objects does not do nested copying
+
+    // Example: Add a new object property and value without mutating the original object
+    const spreadableObject = {
+        name: 'Robert',
+        phone: 'iPhone'
+    };
+
+    const newObject = {
+        ...spreadableObject,
+        carModel: 'Volkswagen'
+    }
+    // newObject will be equal to
+    // { carModel: 'Volkswagen', name: 'Robert', phone: 'iPhone' }
+}
+
+
+{ // FUNCTION REST
+    // Functions can use the rest parameter syntax to accept any number of arguments as an array.
+
+    // Example: Display the array of passed arguments
+    function displayArgumentsArray(...theArguments) {
+        console.log(theArguments);
+    }
+
+    displayArgumentsArray('hi', 'there', 'bud');
+    // Will print ['hi', 'there', 'bud']
+}
