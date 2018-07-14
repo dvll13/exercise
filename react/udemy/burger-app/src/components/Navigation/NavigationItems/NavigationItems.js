7e46d11d4d6505f899c374d5664d013f85cee5d4
Marin <marin_stephanov@abv.bg>
2018-07-14 22:46:18 +0300

    burger-app: L309 (auth)

--- react/udemy/burger-app/src/components/Navigation/NavigationItems/NavigationItems.js
+++ react/udemy/burger-app/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,7 @@ const NavigationItems = (props) => (
         <ul>
             <NavigationItem link='/' exact>Burger Builder</NavigationItem>
             <NavigationItem link='/orders'>Orders</NavigationItem>
+            <NavigationItem link='/auth'>Authenticate</NavigationItem>
         </ul>
     </nav>
 );
---
+++ react/udemy/burger-app/src/containers/Auth/Auth.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+
+import classes from './Auth.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email Address'
+                },
+                value: '',
+                validationRules: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validationRules: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required && isValid) {
+            isValid = value.trim() !== '';
+        }
+
+        if (rules.minLength && isValid) {
+            isValid = value.length >= rules.minLength;
+        }
+
+        if (rules.maxLength && isValid) {
+            isValid = value.length <= rules.maxLength;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validationRules),
+                touched: true
+            }
+        };
+
+        this.setState({ controls: updatedControls });
+    };
+
+    render() {
+
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            });
+        }
+
+        const form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validationRules}
+                touched={formElement.config.touched}
+                valueType={formElement.config.valueType}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+        ))
+
+        return (
+            <div className={classes.Auth}>
+                <form>
+                    {form}
+                    <Button btnType='Success'>SUBMIT</Button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Auth;
