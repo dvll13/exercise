a573dff01150e9e97e3c750346f3831e5328a0d9
Marin <marin_stephanov@abv.bg>
2018-07-21 00:10:44 +0300

    burger-app: L310 (auth)

--- react/udemy/burger-app/src/containers/Auth/Auth.js
+++ react/udemy/burger-app/src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import classes from './Auth.css';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions'
 
 class Auth extends Component {
     state = {
@@ -70,6 +72,11 @@ class Auth extends Component {
         this.setState({ controls: updatedControls });
     };
 
+    submitHandler = (event) => {
+        event.preventDefault(); // prevent the reloading of the page
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
 
         const formElementsArray = [];
@@ -95,7 +102,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>
                 </form>
@@ -104,4 +111,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
