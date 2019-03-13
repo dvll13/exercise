6643493eb01c8dcd33b83a02d5f2ffa9c4985057
Marin <Marin.Stefanov@brandmaker.com>
2019-03-13 23:52:54 +0200

    hooks comment added

--- react/hooks/src/useToggle.js
+++ react/hooks/src/useToggle.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react'
 
 export const useToggle = (initialValue) => {
     const [toggleValue, setToggleValue] = useState(initialValue)
-    const toggler = () => setToggleValue(!toggleValue)
+    //Though creating functions at each render is not slow in modern browsers we can avoid that by memoizing the toggler function (https://reactjs.org/docs/hooks-faq.html#are-hooks-slow-because-of-creating-functions-in-render). For this purpose useCallback hook comes in handy. More details can be found on https://reactjs.org/docs/hooks-reference.html#usecallback.
+    const toggler = () => useCallback(() => setToggleValue(!toggleValue))
 
     return [toggleValue, toggler]
 }
