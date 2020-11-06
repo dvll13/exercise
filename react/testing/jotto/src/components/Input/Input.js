fd5338a4162494b0d6aceb6a3a5e057a9ad323ed
Marin Stefanov <marin.stefanov@brandmaker.com>
2020-11-06 12:12:26 +0200

    new tests and fixed typos

---
+++ react/testing/jotto/src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+function Input(props) {
+    return (
+        <div>
+            <button />
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {}
+}
+
+export default connect(mapStateToProps)(Input)
---
+++ react/testing/jotto/src/components/Input/Input.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import {findByTestAttr, storeFactory} from '../../../test/testUtils'
+import Input from './Input'
+
+const setup = (initialState = {}) => {
+    const store = storeFactory(initialState)
+    const wrapper = shallow(<Input store={store} />)
+        .dive()
+        .dive()
+}
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        it('renders component without error', () => {})
+        it('renders input box', () => {})
+        it('renders submit button', () => {})
+    })
+
+    describe('word has been guessed', () => {
+        it('renders component without error', () => {})
+        it('does not render input box', () => {})
+        it('does not render submit button', () => {})
+    })
+})
+
+describe('update state', () => {
+    it('', () => {})
+})
