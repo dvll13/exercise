542a747104f76e1e9e5a713e2775cb2c3ea8903c
Marin Stefanov <marin.stefanov@brandmaker.com>
2021-05-11 16:21:45 +0300

    mern course init

---
+++ node_react/node-basic/app.js
@@ -0,0 +1,6 @@
+const helpers = require('./helpers')
+
+// console.log('hello world')
+
+const total = helpers.sum(10, 50)
+console.log('TOTAL:', total)
---
+++ node_react/node-basic/helpers.js
@@ -0,0 +1,10 @@
+// const sum = (a, b) => a + b
+exports.sum = (a, b) => a + b
+
+
+// module.exports = {
+//     sum
+// }
+
+// console.log(process.mainModule.Module.exports)
+// -> {sum: [Function: sum]}
