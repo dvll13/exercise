6869bedeb949ed4934bb6d4cec8a232c1649c042
Marin Stefanov <marin.stefanov@brandmaker.com>
2021-11-17 17:00:22 +0200

    react-testing-library classes completed

---
+++ node_react/node-basic/import_modules.js
@@ -0,0 +1,8 @@
+// const helpers = require('./helpers')
+// const total = helpers.sum(10, 50)
+const {sum} = require('./helpers')
+const total = sum(10, 50)
+
+// console.log('hello world')
+
+console.log('TOTAL:', total)
